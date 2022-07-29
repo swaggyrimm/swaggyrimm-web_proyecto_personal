dfbf8d0d74f12d15aa33d7615055bbdf82046e19
JORIM.WILSON@ucr.ac.cr <JORIM.WILSON@ucr.ac.cr>
2022-07-28 20:16:07 -0600

    trying out

--- wp-config.php
+++ wp-config.php
@@ -44,7 +44,7 @@ define( 'DB_COLLATE', '' );
  * the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}.
  *
  * You can change these at any point in time to invalidate all existing cookies.
- * This will force all users to have to log in again.
+ * This will force all users to have to log in again. 
  *
  * @since 2.6.0
  */
